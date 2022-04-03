ce57836ffd8834de9ffd1efdc55be0116bdce855
KhaiND007 <kn20516@gmail.com>
2022-04-03 09:09:22 +0700

    Finish

--- Intent/app/src/main/java/khai/intent/HomeActivity.java
+++ Intent/app/src/main/java/khai/intent/HomeActivity.java
@@ -2,13 +2,45 @@ package khai.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    private TextView tvName;
+    private Button btnQuiz1, btnQuiz2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tvName = (TextView) findViewById(R.id.tvUserName);
+        btnQuiz1 = (Button) findViewById(R.id.btnQuiz1);
+        btnQuiz2 = (Button) findViewById(R.id.btnQuiz2);
+
+        Intent i = getIntent();
+        String UserName = i.getStringExtra("Name");
+
+        tvName.setText(UserName);
+
+        btnQuiz1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iQuiz1 = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(iQuiz1);
+            }
+        });
+
+        btnQuiz2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iQuiz2 = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(iQuiz2);
+            }
+        });
     }
 }
--- Intent/app/src/main/java/khai/intent/MainActivity.java
+++ Intent/app/src/main/java/khai/intent/MainActivity.java
@@ -2,13 +2,23 @@ package khai.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+    }
+    public void Chuyen_Man_Hinh_Dang_Nhap(View v) {
+        Intent iLogin = new Intent(this, LoginActivity.class);
+        startActivity(iLogin);
     }
 }
